80754f027079421b3fbefa2a8616a5e2c8fcb881
Rawand Rebwar <rawandrebwar2010@gmail.com>
2022-12-26 23:22:34 +0300

    set new url

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const config = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://url-shortner-rr1.vercel.app/",
 });
 
 export default config;
--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     JWT_SECRET: "Rawand123",
     MONGO_URI:
       "mongodb+srv://My1stDB:Messi2011@cluster0.er8jm.mongodb.net/url-shortner?retryWrites=true&w=majority",
+    development: "production",
   },
 };
 
